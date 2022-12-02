5dfd3d2e88c2b594b70719dcc4560e053db5bf2f
trishitdedhia <hey@trishitdedhia.com>
2022-12-02 13:05:40 +0530

    renamed files

---
+++ replace.py
@@ -0,0 +1,68 @@
+#!/bin/python
+from PIL import Image
+import sys
+
+#Get image
+img = Image.open(sys.argv[1])
+
+#Convert image to rgb
+img = img.convert('RGBA')
+
+
+#Get old pixels
+print("What are the RGB values of the pixels you want to repaint?")
+rr=int(input(f'R (0-255) : '))
+gg=int(input(f'G (0-255) : '))
+bb=int(input(f'B (0-255) : '))
+#aa=input(f'A (0-1) : ')
+
+
+#Get old pixels
+print("What are the RGBA values of the new paint?")
+r=int(input(f'R (0-255) : '))
+g=int(input(f'G (0-255) : '))
+b=int(input(f'B (0-255) : '))
+a=int(input(f'A (0-255) : '))
+
+#Get image data
+data = img.getdata()
+newdata = []
+
+#Set variables for the loops
+total = img.width * img.height
+sum = 0
+progress = 0
+
+#Insert Progress
+print(f'Processing Image... 0%', end="")
+
+#Run a loop for each pixel
+for px in data:
+    #print(px[0])
+    #print(px[1])
+    #print(px[2])
+
+    #Check if the pixel matches
+    if px[0] == rr and px[1] == gg and px[2] == bb:
+        newdata.append((r, g, b, a))
+        sum += 1
+    else:
+        newdata.append(px)
+    
+    #Increment Progress and print
+    progress += 1
+    percent = int((progress / total) * 100)
+    print(f'\rProcessing Image... {percent}%', end="")
+
+#End process Image
+print("\nImage Processed.")
+
+#Put data and save image
+img.putdata(newdata)
+img.save('output.png')
+
+
+
+
+
+
